8fbff52a4e189fed1c6b381699b027b416d70901
k-jaehyun <wogus146@gmail.com>
2023-11-25 23:50:18 +0900

    회원가입 Validation 예외처리

--- src/main/java/com/sparta/todolist/controller/UserController.java
+++ src/main/java/com/sparta/todolist/controller/UserController.java
@@ -4,9 +4,16 @@ import com.sparta.todolist.dto.LoginRequestDto;
 import com.sparta.todolist.dto.SignupRequestDto;
 import com.sparta.todolist.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -15,13 +22,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user/signup")
-    public String signup(@RequestBody SignupRequestDto requestDto) {
-        System.out.println("sugnup 들어옴");
+    public String signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "허용된 username 또는 password 값이 아닙니다.";
+        }
 
         userService.signup(requestDto);
 
-
-        return "성공?";
+        return "회원가입 성공";
     }
 
 //    @PostMapping("/user/login")
--- src/main/java/com/sparta/todolist/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/todolist/dto/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta.todolist.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,7 +12,9 @@ import lombok.Setter;
 @NoArgsConstructor
 public class SignupRequestDto {
     @NotBlank
+    @Pattern(regexp = "^[a-z0-9]{4,10}$")
     private String username;
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$")
     private String password;
 }
