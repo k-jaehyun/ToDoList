4d03365d2407c7d9f3c60f7e7e2a4f178d576d51
k-jaehyun <wogus146@gmail.com>
2023-12-03 00:21:51 +0900

    refactor(JpaAuditing): made JpaConfig class

--- src/main/java/com/sparta/todolist/ToDoListApplication.java
+++ src/main/java/com/sparta/todolist/ToDoListApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class ToDoListApplication {
 
 	public static void main(String[] args) {
